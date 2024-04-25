7ca6d3aa821e65e0026d94ef43b9a7b7d714c447
fadhilhfz <fadhilhfz369@gmail.com>
2024-04-25 21:05:44 +0700

    add rotate in animation, but still buggy

--- src/SvgPopper.tsx
+++ src/SvgPopper.tsx
@@ -1,9 +1,33 @@
+import { useEffect, useState } from "react";
 import briefcase from "./assets/briefcase.svg";
 
 function SvgPopper() {
+  const [randomNumber, setRandomNumber] = useState<number>(0);
+  const rotate = `rotate(${randomNumber}deg)`;
+  console.log(rotate);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const min = -360;
+      const max = 360;
+      const rnumber = Math.floor(Math.random() * (max - min + 1)) + min;
+      setRandomNumber(rnumber);
+      console.log("effect fired", rnumber, rotate);
+    }, 2250);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
   return (
     <div>
-      <img src={briefcase} alt="briefcase" className="w-32 animate-fadeInOut" />
+      <img
+        src={briefcase}
+        alt="briefcase"
+        style={{ transform: rotate }}
+        className={`w-32 animate-fadeInOut`}
+      />
     </div>
   );
 }
